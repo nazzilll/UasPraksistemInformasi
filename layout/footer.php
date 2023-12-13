4960c6bdb39195275248bb0f31d31465242488c0
nazzill <nazzilakfa@gmail.com>
2023-12-13 21:57:50 +0700

    first init github

---
+++ layout/footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="float-start">
+            <p>2023 &copy; SPK - SAW Method</p>
+        </div>
+        <div class="float-end">
+            <p>Created <span class="text-danger"></i></span> by <a>Nazzil, Ihwan dan Hafidz</a></p>
+        </div>
+    </div>
+</footer>
